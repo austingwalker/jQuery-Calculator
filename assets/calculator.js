380d8aa4bc983171b957a97291236ee3e7a348f8
World Wide <agwalker249@gmail.com>
2018-11-21 21:38:56 -0600

    Condensed code

--- assets/calculator.js
+++ assets/calculator.js
@@ -2,384 +2,512 @@ $(document).ready(function() {
 
     // Global Variables
     
-      var firstNum = $("#first-number");
-      var secondNum = $("#second-number");
-      var operator = $("#operator");
-      var total = $("#result");
+    var firstNumber = "";
+    var secondNumber = "";
+    var operator = "";
+    var total = "";
     
-    //Number buttons -----------------------------------------------------------------------------------
-      var numOneArr = [];
-      var numTwoArr = [];
-      var operatorArr = [];
-      var resultsArr = [];
     
-     function isEmpty( el ){
-          return !$.trim(el.html())
-      }
+    
+    var resultsFirstNum = $("#first-number");
+    var resultsSecondNum = $("#second-number");
+    var resultsOperator = $("#operator")
+    var resultsTotal = $("#result");
+    
+    var operatorClicked = false;
+    var equalsClicked = false;
+    
+    $(".number").on("click", function(){
+    
+        
+    
+        if(!operatorClicked){
+    
+            firstNumber = firstNumber + this.value;
+            resultsFirstNum.text(firstNumber);
+    
+        }
+    
+        else {
+    
+            if(!equalsClicked){
+            secondNumber = secondNumber + this.value;
+            resultsSecondNum.text(secondNumber);
+        }
+        }
+    
+    
+    })
+    
+    $(".operator").on("click", function(){
+    
+        if(!operatorClicked){
+    
+        operator = this.value;
+    
+        resultsOperator.text(operator);
+    
+        operatorClicked = true;
+    
+        }
+    
+        
+    })
+    
+    $(".equal").on("click", function(){
+    
+        var sum1 = parseInt(firstNumber);
+        var sum2 = parseInt(secondNumber);
+    
+        if (operator === "+"){
+            total = sum1 + sum2;
+    
+            resultsTotal.text(total);
+        }
+    
+        else if (operator === "-"){
+            
+            total = sum1 - sum2;
+    
+            resultsTotal.text(total);
+        }
+    
+        else if (operator === "x"){
+    
+            total = sum1 * sum2;
+    
+            resultsTotal.text(total);
+    
+        }
+    
+        else if (operator === "/"){
+    
+        total = sum1 / sum2;
+    
+        resultsTotal.text(total);
+    
+    }
+    
+    else if (operator === "^"){
+    
+    total = sum1 ** sum2;
+    
+    resultsTotal.text(total);
+    
+    }
+    
+        equalsClicked = true;
+    
+    
+    
+    })
+    
+    $(".clear").on("click", function(){
+    
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    total = "";
+    
+    resultsFirstNum.empty();
+    resultsSecondNum.empty();
+    resultsOperator.empty();
+    resultsTotal.empty();
+    
+    operatorClicked = false;
+    equalsClicked = false;
+    
+    
+    })
+    
+    
+    
+    })
+
+//First draft below ------------------------------------------------------------------
+
+// $(document).ready(function() {
+
+//     // Global Variables
+    
+//       var firstNum = $("#first-number");
+//       var secondNum = $("#second-number");
+//       var operator = $("#operator");
+//       var total = $("#result");
+    
+//     //Number buttons -----------------------------------------------------------------------------------
+//       var numOneArr = [];
+//       var numTwoArr = [];
+//       var operatorArr = [];
+//       var resultsArr = [];
+    
+//      function isEmpty( el ){
+//           return !$.trim(el.html())
+//       }
     
       
     
-    $("#button-1").on("click", function(){
-      var one = 1;
+//     $("#button-1").on("click", function(){
+//       var one = 1;
       
     
-     if(isEmpty(operator)){
-       numOneArr.push(one)
-      firstNum.append(one);
+//      if(isEmpty(operator)){
+//        numOneArr.push(one)
+//       firstNum.append(one);
     
-      console.log(numOneArr);
-     } else {
-       numTwoArr.push(one);
-       secondNum.append(one);
+//       console.log(numOneArr);
+//      } else {
+//        numTwoArr.push(one);
+//        secondNum.append(one);
     
-       console.log(numTwoArr);
-     }
+//        console.log(numTwoArr);
+//      }
      
       
-    })
+//     })
     
-    $("#button-2").on("click", function(){
-      var two = 2;
+//     $("#button-2").on("click", function(){
+//       var two = 2;
     
-       if(isEmpty(operator)){
-         numOneArr.push(two);
-      firstNum.append(two);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(two);
-       secondNum.append(two);
+//        if(isEmpty(operator)){
+//          numOneArr.push(two);
+//       firstNum.append(two);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(two);
+//        secondNum.append(two);
     
-       console.log(numTwoArr);
-     }
-    
-      
-    })
-    
-    $("#button-3").on("click", function(){
-      var three = 3;
-      
-      if(isEmpty(operator)){
-        numOneArr.push(three);
-      firstNum.append(three);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(three);
-       secondNum.append(three);
-    
-       console.log(numTwoArr);
-     }
-    
-    })
-    
-    $("#button-4").on("click", function(){
-      var four = 4;
-    
-       if(isEmpty(operator)){
-        numOneArr.push(four);
-      firstNum.append(four);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(four);
-       secondNum.append(four);
-    
-       console.log(numTwoArr);
-     }
+//        console.log(numTwoArr);
+//      }
     
       
-    })
+//     })
     
-    $("#button-5").on("click", function(){
-      var five = 5;
+//     $("#button-3").on("click", function(){
+//       var three = 3;
+      
+//       if(isEmpty(operator)){
+//         numOneArr.push(three);
+//       firstNum.append(three);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(three);
+//        secondNum.append(three);
     
-       if(isEmpty(operator)){
-        numOneArr.push(five)
-      firstNum.append(five);
-      console.log(numOneArr);
-     } else {
-       numTwoArr.push(five);
-       secondNum.append(five);
+//        console.log(numTwoArr);
+//      }
     
-       console.log(numTwoArr);
-     }
+//     })
+    
+//     $("#button-4").on("click", function(){
+//       var four = 4;
+    
+//        if(isEmpty(operator)){
+//         numOneArr.push(four);
+//       firstNum.append(four);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(four);
+//        secondNum.append(four);
+    
+//        console.log(numTwoArr);
+//      }
     
       
-    })
+//     })
     
-    $("#button-6").on("click", function(){
-      var six = 6;
+//     $("#button-5").on("click", function(){
+//       var five = 5;
     
-       if(isEmpty(operator)){
-        numOneArr.push(six);
-      firstNum.append(six);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(six);
-       secondNum.append(six);
+//        if(isEmpty(operator)){
+//         numOneArr.push(five)
+//       firstNum.append(five);
+//       console.log(numOneArr);
+//      } else {
+//        numTwoArr.push(five);
+//        secondNum.append(five);
     
-       console.log(numTwoArr);
-     }
-      
-    })
-    
-    $("#button-7").on("click", function(){
-      var seven = 7;
-    
-       if(isEmpty(operator)){
-        numOneArr.push(seven);
-      firstNum.append(seven);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(seven);
-       secondNum.append(seven);
-    
-       console.log(numTwoArr);
-     }
-      
-    })
-    
-    $("#button-8").on("click", function(){
-      var eight = 8;
-    
-       if(isEmpty(operator)){
-        numOneArr.push(eight);
-      firstNum.append(eight);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(eight);
-       secondNum.append(eight);
-    
-       console.log(numTwoArr);
-     }
+//        console.log(numTwoArr);
+//      }
     
       
-    })
+//     })
     
-    $("#button-9").on("click", function(){
-      var nine = 9;
+//     $("#button-6").on("click", function(){
+//       var six = 6;
     
-       if(isEmpty(operator)){
-        numOneArr.push(nine);
-      firstNum.append(nine);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(nine);
-       secondNum.append(nine);
+//        if(isEmpty(operator)){
+//         numOneArr.push(six);
+//       firstNum.append(six);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(six);
+//        secondNum.append(six);
     
-       console.log(numTwoArr);
-     }
+//        console.log(numTwoArr);
+//      }
+      
+//     })
+    
+//     $("#button-7").on("click", function(){
+//       var seven = 7;
+    
+//        if(isEmpty(operator)){
+//         numOneArr.push(seven);
+//       firstNum.append(seven);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(seven);
+//        secondNum.append(seven);
+    
+//        console.log(numTwoArr);
+//      }
+      
+//     })
+    
+//     $("#button-8").on("click", function(){
+//       var eight = 8;
+    
+//        if(isEmpty(operator)){
+//         numOneArr.push(eight);
+//       firstNum.append(eight);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(eight);
+//        secondNum.append(eight);
+    
+//        console.log(numTwoArr);
+//      }
     
       
-    })
+//     })
     
-    $("#button-0").on("click", function(){
-      var zero = 0;
+//     $("#button-9").on("click", function(){
+//       var nine = 9;
     
-      if(isEmpty(operator)){
-        numOneArr.push(zero);
-      firstNum.append(zero);
-      console.log(numOneArr);
-     } else {
-      numTwoArr.push(zero);
-       secondNum.append(zero);
+//        if(isEmpty(operator)){
+//         numOneArr.push(nine);
+//       firstNum.append(nine);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(nine);
+//        secondNum.append(nine);
     
-       console.log(numTwoArr);
-     }
+//        console.log(numTwoArr);
+//      }
     
       
-    })
+//     })
+    
+//     $("#button-0").on("click", function(){
+//       var zero = 0;
+    
+//       if(isEmpty(operator)){
+//         numOneArr.push(zero);
+//       firstNum.append(zero);
+//       console.log(numOneArr);
+//      } else {
+//       numTwoArr.push(zero);
+//        secondNum.append(zero);
+    
+//        console.log(numTwoArr);
+//      }
+    
+      
+//     })
 
-    //Operators --------------------------------------------------------------
+//     //Operators --------------------------------------------------------------
 
-$("#button-plus").on("click", function(){
-    var plus = "+";
+// $("#button-plus").on("click", function(){
+//     var plus = "+";
   
     
-  if(operatorArr.length >= 1){
-    return;
-  }
-   if(isEmpty(firstNum)){
-    return;
-   } else {
-     operatorArr.push(plus);
-     operator.append(plus);
-   }
+//   if(operatorArr.length >= 1){
+//     return;
+//   }
+//    if(isEmpty(firstNum)){
+//     return;
+//    } else {
+//      operatorArr.push(plus);
+//      operator.append(plus);
+//    }
   
     
-  })
+//   })
   
-  $("#button-minus").on("click", function(){
-    var minus = "-";
+//   $("#button-minus").on("click", function(){
+//     var minus = "-";
   
-    if(operatorArr.length >= 1){
-    return;
-  }
+//     if(operatorArr.length >= 1){
+//     return;
+//   }
   
-    if(isEmpty(firstNum)){
-    return;
-   } else {
-    operatorArr.push(minus);
-     operator.append(minus);
-   }
-  
-    
-  })
-  
-  $("#button-multiply").on("click", function(){
-    var multiply = "x";
-  
-    if(operatorArr.length >= 1){
-    return;
-  }
-  
-    if(isEmpty(firstNum)){
-    return;
-   } else {
-    operatorArr.push(multiply);
-     operator.append(multiply);
-   }
+//     if(isEmpty(firstNum)){
+//     return;
+//    } else {
+//     operatorArr.push(minus);
+//      operator.append(minus);
+//    }
   
     
-  })
+//   })
   
-  $("#button-divide").on("click", function(){
-    var divide = "%";
+//   $("#button-multiply").on("click", function(){
+//     var multiply = "x";
   
-    if(operatorArr.length >= 1){
-    return;
-  }
+//     if(operatorArr.length >= 1){
+//     return;
+//   }
   
-  if(isEmpty(firstNum)){
-    return;
-   } else {
-    operatorArr.push(divide);
-     operator.append(divide);
-   }
-  
-    
-  })
-  
-  $("#button-power").on("click", function(){
-    var power = "^";
-  
-    if(operatorArr.length >= 1){
-    return;
-  }
-  
-   if(isEmpty(firstNum)){
-    return;
-   } else {
-    operatorArr.push(power);
-     operator.append(power);
-   }
+//     if(isEmpty(firstNum)){
+//     return;
+//    } else {
+//     operatorArr.push(multiply);
+//      operator.append(multiply);
+//    }
   
     
-  })
+//   })
   
-  $("#button-equal").on("click", function(){
-    var result = 0;
+//   $("#button-divide").on("click", function(){
+//     var divide = "%";
   
-    
-     var sum1 = numOneArr.join('');
-     var sum2 = numTwoArr.join('');
+//     if(operatorArr.length >= 1){
+//     return;
+//   }
   
-     var numSum1 = parseInt(sum1);
-     var numSum2 = parseInt(sum2);
-  
-  
-  
-  console.log("-------------------");
-  
-  console.log(operatorArr);
-  
-  console.log("--------------------");
-  
-  // Results for addition ---------------------------------------------
-  if(resultsArr.length >= 1){
-    return;
-  }
-  if (operatorArr[0] === "+"){
-    resultsArr.push(result);
-    result = numSum1+numSum2;
-  
-  
-    total.append("= " + result);
+//   if(isEmpty(firstNum)){
+//     return;
+//    } else {
+//     operatorArr.push(divide);
+//      operator.append(divide);
+//    }
   
     
+//   })
   
-  // Results for subtraction --------------------------------------------- 
-  }
+//   $("#button-power").on("click", function(){
+//     var power = "^";
   
-  else if (operatorArr[0] === "-"){
-    resultsArr.push(result);
-    result = numSum1-numSum2;
+//     if(operatorArr.length >= 1){
+//     return;
+//   }
   
-  
-    total.append("= " + result);
-  
-    
-  }
-  
-  // Results for multiplication  ---------------------------------------------
-  
-  else if (operatorArr[0] === "x"){
-    resultsArr.push(result);
-    result = numSum1*numSum2;
-  
-  
-    total.append("= " + result);
+//    if(isEmpty(firstNum)){
+//     return;
+//    } else {
+//     operatorArr.push(power);
+//      operator.append(power);
+//    }
   
     
-  }
+//   })
   
-  // Results for division ---------------------------------------------
-  
-  else if (operatorArr[0] === "%"){
-    resultsArr.push(result);
-    result = numSum1/numSum2;
-  
-  
-    total.append("= " + result);
+//   $("#button-equal").on("click", function(){
+//     var result = 0;
   
     
-  }
+//      var sum1 = numOneArr.join('');
+//      var sum2 = numTwoArr.join('');
   
-  // Results for power ---------------------------------------------
-  
-  else if (operatorArr[0] === "^"){
-    resultsArr.push(result);
-    result = numSum1**numSum2;
-  
-  
-    total.append("= " + result);
-  
-    
-  }
-    
-  
-    console.log(sum1);
-    console.log(sum2);
-    console.log(result);
+//      var numSum1 = parseInt(sum1);
+//      var numSum2 = parseInt(sum2);
   
   
   
-    
-  })
+//   console.log("-------------------");
+  
+//   console.log(operatorArr);
+  
+//   console.log("--------------------");
+  
+//   // Results for addition ---------------------------------------------
+//   if(resultsArr.length >= 1){
+//     return;
+//   }
+//   if (operatorArr[0] === "+"){
+//     resultsArr.push(result);
+//     result = numSum1+numSum2;
   
   
-  $("#button-clear").on("click", function(){
-    
-  // Empty div's -------------------------------------------
-    firstNum.empty();
-    operator.empty();
-    secondNum.empty();
-    total.empty();
-  // Empty array's -------------------------------------
-    numOneArr = [];
-    numTwoArr = [];
-    operatorArr = [];
-    resultsArr = [];
+//     total.append("= " + result);
   
     
-  })
+  
+//   // Results for subtraction --------------------------------------------- 
+//   }
+  
+//   else if (operatorArr[0] === "-"){
+//     resultsArr.push(result);
+//     result = numSum1-numSum2;
+  
+  
+//     total.append("= " + result);
+  
+    
+//   }
+  
+//   // Results for multiplication  ---------------------------------------------
+  
+//   else if (operatorArr[0] === "x"){
+//     resultsArr.push(result);
+//     result = numSum1*numSum2;
+  
+  
+//     total.append("= " + result);
+  
+    
+//   }
+  
+//   // Results for division ---------------------------------------------
+  
+//   else if (operatorArr[0] === "%"){
+//     resultsArr.push(result);
+//     result = numSum1/numSum2;
+  
+  
+//     total.append("= " + result);
+  
+    
+//   }
+  
+//   // Results for power ---------------------------------------------
+  
+//   else if (operatorArr[0] === "^"){
+//     resultsArr.push(result);
+//     result = numSum1**numSum2;
+  
+  
+//     total.append("= " + result);
+  
+    
+//   }
+    
+  
+//     console.log(sum1);
+//     console.log(sum2);
+//     console.log(result);
+  
+  
+  
+    
+//   })
+  
+  
+//   $("#button-clear").on("click", function(){
+    
+//   // Empty div's -------------------------------------------
+//     firstNum.empty();
+//     operator.empty();
+//     secondNum.empty();
+//     total.empty();
+//   // Empty array's -------------------------------------
+//     numOneArr = [];
+//     numTwoArr = [];
+//     operatorArr = [];
+//     resultsArr = [];
+  
+    
+//   })
 
-});
+// });
